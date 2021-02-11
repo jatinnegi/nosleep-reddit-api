fe790d0fb69a7f93dec538d9ba0768e43200f187
Jatin Negi <jatin.07star@gmail.com>
2021-02-11 20:04:55 +0530

    Refactoring

--- static/js/index.js
+++ static/js/index.js
@@ -143,7 +143,7 @@ function getSearchSubmissions(search, page) {
 
   fetch(`https://nosleep-reddit-api.herokuapp.com/search/`, {
     method: "POST",
-    credentials: "same-origin",
+    // credentials: "same-origin",
     // mode: "cors",
     // cache: "no-cache",
     headers: {
